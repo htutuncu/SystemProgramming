933c01cf1138c5e437c3315a60b6794ef0b6f283
Hikmet Tutuncu <48223977+htutuncu@users.noreply.github.com>
2021-05-16 01:58:00 -0700

    Kilit dosyasının okunma şekli düzeltildi.
    Daha önce kilit dosyasını fields kullanarak okumamıştım. Bu düzenleme ile fields kullanılarak okudum.
    Memory leak düzeltildi.

--- v1.0/main.c
+++ v1.0/main.c
@@ -7,156 +7,135 @@
 #include "dllist.h"
 #include "fields.h"
 
-#define MAX_SIZE 10000
+#define MAX_SIZE 1000
 
 char *removeLeadingSpaces(char *str);
 
-void removeChar(char *s, char c);
-
 int compareJval(Jval j1, Jval j2);
-char *removeColons(char *str);
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 
-    if(argc != 4){  /* Checking if program called properly */
+    if (argc != 4)
+    { /* Checking if program called properly */
         fprintf(stderr, "Incorrect parameter count\n");
         exit(EXIT_FAILURE);
-    } 
+    }
 
     JRB jrb = make_jrb();
 
     const char *filename = ".kilit";
-    FILE *fp = fopen(filename, "rb");
+    //FILE *fp = fopen(filename, "rb");
 
-    IS is;      /* Fields struct for reading input */
+    IS is; /* Fields struct for reading input */
+    IS kilit;
+    kilit = new_inputstruct(filename);
     is = new_inputstruct(argv[2]);
-    if (is == NULL) {
+    if (is == NULL)
+    {
         perror(argv[2]);
         exit(1);
     }
-
-    FILE * output = fopen(argv[3], "w+");
-
-    if (!fp)
-        perror(filename), exit(1);
-
-    int bufferLength = MAX_SIZE;
-    char buffer[bufferLength];
-    char *file[MAX_SIZE];
-    int i = 0;
-    for (i = 0; i < MAX_SIZE; i++)
-        file[i] = (char *)malloc(MAX_SIZE * sizeof(char));
-
-    i = 0;
-    while (fgets(buffer, bufferLength, fp))
-        strcpy(file[i++], buffer);
-    
-
-    int j;
-    
-    for (j = 1; j < i - 1; j++)
+    if (kilit == NULL)
     {
-        file[j] = removeLeadingSpaces(file[j]);
-        //removeChar(file[j], ',');
-        //file[j] = removeColons(file[j]);
-        //file[j][strlen(file[j])-2] = '\0';
-        //printf("%s\n",file[j]);
-
-        char *token = strtok(file[j], ":");
-        //removeLeadingSpaces(token);
-
-        /* Keep printing tokens while one of the
-           delimiters present in str[]. */
-
-        char *key = strdup(token);
-        int k;
-        char *newKey = (char*)malloc(strlen(key)*sizeof(char));
-        for(k=0; k<strlen(key)-2; k++){
-            newKey[k] = key[k+1];
-        }
-        
-        
-        
-        token = strtok(NULL, ":");
-
-        char *value = strdup(token);
-        value = removeLeadingSpaces(value);
-        char *newValue = (char*)malloc(strlen(value)*sizeof(char));
-        for(k=0; k<strlen(value)-3; k++){
-            newValue[k] = value[k+1];
-        }
-        
-        token = strtok(NULL, ":");
-
-        if(newValue[strlen(newValue)-1] == '\"')
-            newValue[strlen(newValue)-1] = '\0';
-        else{
-            newValue[strlen(newValue)] = '\0';
-        }
-
-    
-        
-
-        Jval myJval = (Jval)malloc(sizeof(Jval));
-        myJval = new_jval_v(strdup(newValue));
-
-        Jval myJval2 = (Jval)malloc(sizeof(Jval));
-        myJval2 = new_jval_s(strdup(newKey));
-
-        if(strcmp(argv[1], "-e")==0){
-            (void) jrb_insert_gen(jrb, myJval2, myJval, compareJval);
-        }
-
-        else if(strcmp(argv[1], "-d")== 0){
-
-            (void) jrb_insert_gen(jrb, myJval, myJval2, compareJval);
-        }
-
-        else{
-            fprintf(stderr, "Wrong parameter/n");
-            exit(EXIT_FAILURE);
-        }
-        
-        
-        free(newKey);
-        free(newValue);
+        perror(filename);
+        exit(1);
     }
-    
-    
 
+    FILE *output = fopen(argv[3], "w+");
 
+    int i;
+    while (get_line(kilit) >= 0)
+    {
+        for (i = 0; i < kilit->NF; i++)
+        {
+            if (strcmp(kilit->fields[i], "{") != 0 && strcmp(kilit->fields[i], "}") != 0)
+            {
 
-
-    if(strcmp(argv[1], "-e")==0){
-        while(get_line(is) >= 0) {
-            for (i = 0; i < is->NF; i++) {
-                if(jrb_find_str(jrb, is->fields[i]) != NULL)
-                    fprintf(output,"%s ",jrb_find_str(jrb, is->fields[i])->val);
+                char *key = strdup(kilit->fields[i]);
+                int k;
+                char *newKey = (char *)malloc(strlen(key) * sizeof(char));
+                for (k = 0; k < strlen(key) - 3; k++)
+                {
+                    newKey[k] = key[k + 1];
+                }
+                char *value = strdup(kilit->fields[i + 1]);
+                char *newValue = (char *)malloc(strlen(value) * sizeof(char));
+                for (k = 0; k < strlen(value) - 2; k++)
+                {
+                    newValue[k] = value[k + 1];
+                }
+                if (newValue[strlen(newValue) - 1] == '\"')
+                    newValue[strlen(newValue) - 1] = '\0';
                 else
-                    fprintf(output,"%s ",is->fields[i]);
-                
-            }
-        }
-    }else{
-        while(get_line(is) >= 0) {
-            for (i = 0; i < is->NF; i++) {
-                if(jrb_find_str(jrb, is->fields[i]) != NULL)
-                    fprintf(output,"%s ",jrb_find_str(jrb, is->fields[i])->val);
+                {
+                    newValue[strlen(newValue)] = '\0';
+                }
+
+                Jval myJval = (Jval)malloc(sizeof(Jval));
+                myJval = new_jval_v(strdup(newValue));
+
+                Jval myJval2 = (Jval)malloc(sizeof(Jval));
+                myJval2 = new_jval_s(strdup(newKey));
+
+                if (strcmp(argv[1], "-e") == 0)
+                {
+                    (void)jrb_insert_gen(jrb, myJval2, myJval, compareJval);
+                }
+
+                else if (strcmp(argv[1], "-d") == 0)
+                {
+
+                    (void)jrb_insert_gen(jrb, myJval, myJval2, compareJval);
+                }
+
                 else
-                    fprintf(output,"%s ",is->fields[i]);
+                {
+                    fprintf(stderr, "Wrong parameter/n");
+                    exit(EXIT_FAILURE);
+                }
+
+                free(newKey);
+                free(newValue);
+
+                i++;
             }
         }
     }
 
+    if (strcmp(argv[1], "-e") == 0)
+    {
+        while (get_line(is) >= 0)
+        {
+            for (i = 0; i < is->NF; i++)
+            {
+                if (jrb_find_str(jrb, is->fields[i]) != NULL)
+                    fprintf(output, "%s ", jrb_find_str(jrb, is->fields[i])->val);
+                else
+                    fprintf(output, "%s ", is->fields[i]);
+            }
+        }
+    }
+    else
+    {
+        while (get_line(is) >= 0)
+        {
+            for (i = 0; i < is->NF; i++)
+            {
+                if (jrb_find_str(jrb, is->fields[i]) != NULL)
+                    fprintf(output, "%s ", jrb_find_str(jrb, is->fields[i])->val);
+                else
+                    fprintf(output, "%s ", is->fields[i]);
+            }
+        }
+    }
 
-    fclose(fp);
+    jettison_inputstruct(kilit);
     jettison_inputstruct(is);
     fclose(output);
 
     return 0;
 }
-
 
 char *removeLeadingSpaces(char *str)
 {
@@ -183,30 +162,8 @@ char *removeLeadingSpaces(char *str)
     return str1;
 }
 
-char *removeColons(char *str)
+int compareJval(Jval j1, Jval j2)
 {
-    int count = 0, j, k;
-
-    if( str[strlen(str)-2] == ',')
-        str[strlen(str)-2] = '\0';
-
-    //printf("%s\n",str);
-
-    return str;
-}
-
-void removeChar(char *s, char c)
-{
-
-    int j, n = strlen(s);
-    for (int i = j = 0; i < n; i++)
-        if (s[i] != c)
-            s[j++] = s[i];
-
-    s[j] = '\0';
-}
-
-int compareJval(Jval j1, Jval j2){
 
     return strcmp(j1.s, j2.s);
 }
